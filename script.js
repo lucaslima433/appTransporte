a7b1213b0f86057057197a3645a07b49be2575bf
lucaslima433 <64279587+lucaslima433@users.noreply.github.com>
2020-05-30 18:21:31 -0300

    appTransporte versão 2

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 //função para arrumar a moeda
 function formataMoeda(numeroMoeda){
     return numeroMoeda.toFixed(2).replace(".",",")
-  };
+};
   
-  
-  function calcular(){
+function calcular(){
+  let txtkm = document.querySelector("#inputDistancia")
+  let txtmin = document.querySelector("#inputTempo")
+
+  let km = Number(txtkm.value)
+  let min = Number(txtmin.value)
+
+  if(txtkm.value.length == 0 || txtmin.value.length == 0){
+    alert('[ERROR] Preencha os dados corretamente e tente novamente!')
+
+  }else{
+    //2 + 1.4*km + min*0.26    
     
-    //2 + 1.4*km + min*0.26
-    km = inputDistancia.value
-  
-    min = inputTempo.value
-  
-    valortotal = 2 + 1.4*km + min*0.26
-  
-   botaoCalcular.innerHTML = 'Total: R$'+ formataMoeda(valortotal);
-  };
+    valortotal = 2 + 1.4*km + min*0.26  
+    botaoCalcular.innerHTML = 'Total: R$'+ formataMoeda(valortotal);
+  }
+};
